a27bc8d5ef185d51ccf58c0e221411af2b3520d2
xylophonez <xylophonezygote@gmail.com>
2023-07-25 11:13:45 +0100

    init

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      {/* Footer content, e.g., copyright information */}
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      {/* Header content, such as logo and navigation links */}
+    </header>
+  );
+};
+
+export default Header;
